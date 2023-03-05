92032b45d5c9d96adc44a2e96e58dcd9b07d6629
Siinnn <myaasinrafi25@gmail.com>
2023-03-06 01:47:16 +0700

    first commit

---
+++ TP_MOD3_1302210055/Form1.cs
@@ -0,0 +1,20 @@
+namespace TP_MOD3_1302210055
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.label2.Text = this.textBox1.Text;
+        }
+    }
+}
